318d09b149f4c069feb6a1dc895e684d4cb233d9
GomezGustavoA <gustavo_64_231@hotmail.com>
2024-08-19 17:05:37 -0300

    add index.js configuration for deployment

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import sendMailrouter from "./routes/sendMail.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -16,7 +17,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -30,6 +31,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 app.use("/api/mail", sendMailrouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
